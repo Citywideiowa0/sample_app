001f03fd014020d02222de448e7929b552478690
Citywideiowa0 <masinusa@gmail.com>
2020-09-13 14:00:55 +0000

    touch up website

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   
   # Returns the full title on a per-page basis.
-  def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
+  def full_title(page_title = '')
+    base_title = "Connecting Grinnellians"
     if page_title.empty?
       base_title
     else
