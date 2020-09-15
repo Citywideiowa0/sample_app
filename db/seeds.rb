496d06622f1576fe92082a5caf1924311d4c3aac
Ubuntu <ubuntu@ip-172-31-60-62.ec2.internal>
2020-09-15 23:50:59 +0000

    Hw6: Add Simple Model and Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+Org.create(name: 'Connecting Grinnellians')
+Org.create(name: 'Altbreak')
+Org.create(name: "Ignite")
